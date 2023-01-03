323e69ffb8731a6f053fe73c3ce66145fb4e34cf
Vilma Čivilyte <106765893+VilmaCivilyte@users.noreply.github.com>
2023-01-03 19:02:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
+from modules.math.composition import composition
+from modules.math.division import division
+from modules.math.multiplication import multiplication
+from modules.math.subtraction import substraction
+from modules.numbers.numbers import one, two, three, four, five
+
+# Kitų failų ir žemiau esančio kodo nekeiskite
+a = composition(one, four);
+b = division(four, two);
+c = substraction(three, two);
+d = multiplication(five, two);
+
+print(a);
+print(b);
+print(c);
+print(d);
